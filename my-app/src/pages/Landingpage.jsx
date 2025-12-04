938cc61a72622ccb5e420be3acc2cf0109e67d9b
Akinremi Daniel <oluwajomiloju2650@gmail.com>
2025-12-05 00:59:29 +0100

    .

--- my-app/src/pages/Landingpage.jsx
+++ my-app/src/pages/Landingpage.jsx
@@ -309,7 +309,7 @@ const partners = ['ETHEREUM', 'POLYGON', 'METAMASK', 'COINBASE', 'BINANCE', 'OPE
                 return (
                   <motion.div
                     key={nft.id}
-                    className="absolute top-0 left-1/2 transform -translate-x-2/2 cursor-pointer w-72"
+                    className="absolute top-0 left-1/2 transform -translate-x-1/1 cursor-pointer w-72"
                     style={{ pointerEvents: isCenter ? 'auto' : 'none' }}
                     animate={style}
                     transition={{ type: 'spring', stiffness: 300, damping: 30 }}
